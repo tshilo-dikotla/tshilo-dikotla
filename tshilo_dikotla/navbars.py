75658b2c5aa776864bdcbb2f098f340e6f4f01f5
Coulson Kgathi <ckgathi@gmail.com>
2019-01-02 23:21:32 +0200

    update nave bars to add infant listboard

--- tshilo_dikotla/navbars.py
+++ tshilo_dikotla/navbars.py
@@ -1,16 +1,9 @@
 from django.conf import settings
-# from edc_lab_dashboard.dashboard_urls import dashboard_urls as lab_dashboard_urls
+from edc_lab_dashboard.dashboard_urls import dashboard_urls as lab_dashboard_urls
 from edc_navbar import NavbarItem, site_navbars, Navbar
 
 
 tshilo_dikotla = Navbar(name='tshilo_dikotla')
-
-# tshilo_dikotla.append_item(
-#     NavbarItem(
-#         name='lab',
-#         label='Specimens',
-#         fa_icon='fa-flask',
-#         url_name=lab_dashboard_urls.get('requisition_listboard_url')))
 
 tshilo_dikotla.append_item(
     NavbarItem(
@@ -26,5 +19,20 @@ tshilo_dikotla.append_item(
         fa_icon='far fa-user-circle',
         url_name=settings.DASHBOARD_URL_NAMES.get('subject_listboard_url')))
 
+
+tshilo_dikotla.append_item(
+    NavbarItem(
+        name='infant_subject',
+        title='Infant Subjects',
+        label='Infant Subjects',
+        fa_icon='far fa-user-circle',
+        url_name=settings.DASHBOARD_URL_NAMES.get('infant_listboard_url')))
+
+tshilo_dikotla.append_item(
+    NavbarItem(
+        name='lab',
+        label='Specimens',
+        fa_icon='fa-flask',
+        url_name=lab_dashboard_urls.get('requisition_listboard_url')))
 
 site_navbars.register(tshilo_dikotla)
