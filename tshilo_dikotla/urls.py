38a6f1b00bda20343973d86b210a9cea7d454457
Leslie <leslie.kep@gmail.com>
2019-10-15 18:33:55 +0200

    remove admin url

--- tshilo_dikotla/urls.py
+++ tshilo_dikotla/urls.py
@@ -27,7 +27,7 @@ from .views import HomeView, AdministrationView
 urlpatterns = [
     path('accounts/', include('edc_base.auth.urls')),
     path('admin/', include('edc_base.auth.urls')),
-    path('admin/', admin.site.urls),
+
     path('admin/', td_maternal_admin.urls),
     path('admin/', td_infant_admin.urls),
     path('admin/', admin.site.urls),
